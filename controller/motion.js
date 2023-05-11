ebba215736c399a6df326df9a59b16f0f43ec45d
omal-harsha <omalharshanuwan@gmail.com>
2023-05-11 15:16:02 +0530

    code clean

--- controller/motion.js
+++ controller/motion.js
@@ -1,24 +1,30 @@
 
 
 export const velocity = (req,res) => {
+    let u = ""
+    let a = ""
+    let t = ""
 
     if(req.query.a && req.query.u && req.query.t) {
 
-        const a = Number(req.query.a)
-        const u = Number(req.query.u)
-        const t = Number(req.query.t)
+        a = Number(req.query.a)
+        u = Number(req.query.u)
+        t = Number(req.query.t)
 
-        const v = u + (a*t)
-        res.json("Velocity is = " +v)
     }else{
   
-        const u = req.body.u
-        const a = req.body.a
-        const t = req.body.t
+         u = req.body.u
+         a = req.body.a
+         t = req.body.t
 
+        
+    }
         const v = u + (a*t)
         res.json("Velocity is = " +v)
-    }
-
     
 }
+
+
+
+
+
