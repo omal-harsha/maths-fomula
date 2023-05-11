25879ddc99b5d846751ae88961bc0d11f43002b1
omal-harsha <omalharshanuwan@gmail.com>
2023-05-11 14:37:40 +0530

    update velocity fomula

--- controller/maths.js
+++ controller/maths.js
@@ -1,6 +1,11 @@
 
 export const sum = (req,res) => {
     
-    const sum = req.body.first + req.body.second
+    if(req.query.first && req.query.second) {
+    const sum = Number(req.query.first) + Number(req.query.second)
     res.json("total is " + sum)
+    } else{
+        const sum = req.body.first + req.body.second
+    res.json("total is " + sum)
+    }
 }
--- controller/motion.js
+++ controller/motion.js
@@ -1,12 +1,24 @@
 
 
 export const velocity = (req,res) => {
+
+    if(req.query.a && req.query.u && req.query.t) {
+
+        const a = Number(req.query.a)
+        const u = Number(req.query.u)
+        const t = Number(req.query.t)
+
+        const v = u + (a*t)
+        res.json("Velocity is = " +v)
+    }else{
   
-    const u = req.body.u
-    const a = req.body.a
-    const t = req.body.t
+        const u = req.body.u
+        const a = req.body.a
+        const t = req.body.t
 
-    const v = u + (a*t)
+        const v = u + (a*t)
+        res.json("Velocity is = " +v)
+    }
 
-    res.json("Velocity is = " +v)
+    
 }
